04972c45fc80481aef30255e43614ad20dabcf74
Loic <konanloic10@gmail.com>
2021-04-05 23:26:41 -0500

    README.md

--- GUIbox.py
+++ GUIbox.py
@@ -1,11 +1,23 @@
-import tkinter as tk
-window = tk.Tk()
-frame_a = tk.Frame()
-frame_b = tk.Frame()
-label_a = tk.Label(master=frame_a, text="Frame A")
-label_a.pack()
-label_b = tk.Label(master=frame_b, text="I'm in Frame B")
-label_b.pack()
-frame_a.pack()
-frame_b.pack()
-window.mainloop()
+# import tkinter as tk
+# window = tk.Tk()
+# frame_a = tk.Frame()
+# frame_b = tk.Frame()
+# label_a = tk.Label(master=frame_a, text="Frame A")
+# label_a.pack()
+# label_b = tk.Label(master=frame_b, text="I'm in Frame B")
+# label_b.pack()
+# frame_a.pack()
+# frame_b.pack()
+# window.mainloop()
+
+
+chararcter_name = "John"
+character_age = "70"
+
+
+print("There once was a man named " + chararcter_name)
+print("he was " + character_age + " yrs old. ")
+
+chararcter_name = "Tom"
+print("he really liked the name " + chararcter_name)
+print("but didn't like being 70. ")
