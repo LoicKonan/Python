9c016c42a2822981d765c7bb4aff82c7a67f2cb6
Loic <konanloic10@gmail.com>
2021-10-18 22:48:55 -0500

    README.md

--- Python Projects/A02/project2.py
+++ Python Projects/A02/project2.py
@@ -20,9 +20,11 @@ if __name__=="__main__":
     
     print("Enter a positive number  or enter the letter e to end the program: ")
     
+    # This try block will make will run as long as the input enter a valid number. ie not the character 'E' or 'e'
     try:
                
-        while not(input() == 'e'):
+        # this while loop will only execute if we have a valid number.
+        while True:
             
             # printing the text to the console for number
             i = int(input())
@@ -49,5 +51,6 @@ if __name__=="__main__":
             else:
                 print("number of factors are less than 3 ")
             
+    # this except block will run when the user enter the E or 'e'. and end the program.
     except ValueError:
         print("You should enter a number instead, anyway bye Felicia lol !!!")
