74c1a8c00587dfc06530f424e5def195f0532761
Loic <konanloic10@gmail.com>
2021-11-12 17:26:10 -0600

    README.md

--- Python Projects/A03/main.py
+++ Python Projects/A03/main.py
@@ -1,6 +1,7 @@
 # Import Required Library
 
 from tkinter import*
+from tkinter import font
 
 from PyDictionary import PyDictionary
 
@@ -22,7 +23,7 @@ def dict():
 # Add Labels, Button and Frames
 Label(root, text = "Dictionary", font = ("Helvetica 20 bold"), fg = "Green").pack( pady = 10)
 
-# Frame 1
+# Frame 1st
 frame = Frame(root)
 Label(frame, text = "Type Word", font = ("Helvetica 15 bold")).pack(side = LEFT)
 
@@ -31,3 +32,34 @@ WORD = Entry(frame, font = ("Helvetica 15 bold"))
 word.pack()
 frame.pack( pady = 10)
 
+# Frame 2nd
+frame1 = Frame(root)
+Label(frame1, text = "Meaning:- ", font = ("Helvetica 10 bold")).pack(side = LEFT)
+meaning = Label(frame1, text = "", font = ("Helvetica 10 bold"))
+meaning.pack()
+frame1.pack(pady = 10)
+
+# Frame 3rd
+frame2 = Frame(root)
+Label(frame2, text = "Synonym:- ", font= ("Helvetica 10 bold").pack(side = LEFT))
+synonym = Label(frame2, text = "", font = ("Helvetica 10"))
+synonym.pack()
+frame2.pack( pady = 10)
+
+# Frame 4th
+
+frame1 = Frame(root)
+Label(frame1, text = "Meaning:- ", font = ("Helvetica 10 bold")).pack(side = LEFT)
+meaning = Label(frame1, text = "", font = ("Helvetica 10 bold"))
+meaning.pack()
+frame1.pack(pady = 10)
+
+# Frame 3rd
+frame2 = Frame(root)
+Label(frame2, text = "Synonym:- ", font= ("Helvetica 10 bold").pack(side = LEFT))
+synonym = Label(frame2, text = "", font = ("Helvetica 10"))
+synonym.pack()
+frame2.pack( pady = 10)
+
+# Execute Tkinter 
+root.mainloop()
