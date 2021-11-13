f8ff9ff22ead09c41a03b3edd57791d220e1136e
Loic <konanloic10@gmail.com>
2021-11-12 18:15:20 -0600

    README.md

--- Python Projects/A03/main.py
+++ Python Projects/A03/main.py
@@ -1,76 +1,80 @@
-# Import Required Library
+# # Import Required Library
+# from tkinter import *
 
-from tkinter import*
-from tkinter import font
+# from PyDictionary import PyDictionary
 
-from pydictionary import Dictionary
+# # Create objects
+# dictionary = PyDictionary()
 
-# Create objects
-dictionary = Dictionary()
+# root = Tk()
 
-root = Tk()
+# # Set geometry
+# root.geometry("400X400")
 
-# Set geometry
-root.geometry("400X400")
-
-def dict():
-    meaning.config(text = dictionary.meaning(word.get())['Noun'][0])
-    meaning.config(text = dictionary.meaning(synonym.get()))
-    meaning.config(text = dictionary.meaning(anton.get()))
+# def dict():
+#     meaning.config(text = dictionary.meaning(word.get())['Noun'][0])
+#     meaning.config(text = dictionary.synonym(word.get()))
+#     meaning.config(text = dictionary.antonym(word.get()))
     
-# Add Labels, Button and Frames
-Label(root, text = "Dictionary", font = ("Helvetica 20 bold"), fg = "Green").pack( pady = 10)
+# # Add Labels, Button and Frames
+# Label(root, text = "Dictionary", font = ("Helvetica 20 bold"), fg = "Green").pack( pady = 10)
 
-# Frame 1st
-frame = Frame(root)
-Label(frame, text = "Type Word", font = ("Helvetica 15 bold")).pack(side = LEFT)
+# # Frame 1st
+# frame = Frame(root)
+# Label(frame, text = "Type Word", font = ("Helvetica 15 bold")).pack(side = LEFT)
 
-WORD = Entry(frame, font = ("Helvetica 15 bold"))
+# word = Entry(frame, font = ("Helvetica 15 bold"))
 
-word.pack()
-frame.pack( pady = 10)
+# word.pack()
+# frame.pack( pady = 10)
 
-# Frame 2nd
-frame1 = Frame(root)
-Label(frame1, text = "Meaning:- ", font = ("Helvetica 10 bold")).pack(side = LEFT)
-meaning = Label(frame1, text = "", font = ("Helvetica 10 bold"))
-meaning.pack()
-frame1.pack(pady = 10)
-
-
-# Frame 3rd
-frame2 = Frame(root)
-Label(frame2, text = "Synonym:- ", font= ("Helvetica 10 bold").pack(side = LEFT))
-synonym = Label(frame2, text = "", font = ("Helvetica 10"))
-synonym.pack()
-frame2.pack( pady = 10)
+# # Frame 2nd
+# frame1 = Frame(root)
+# Label(frame1, text = "Meaning:- ", font = ("Helvetica 10 bold")).pack(side = LEFT)
+# meaning = Label(frame1, text = "", font = ("Helvetica 10 bold"))
+# meaning.pack()
+# frame1.pack(pady = 10)
 
 
-# Frame 4th
-frame3 = Frame(root)
-Label(frame3, text = "Meaning:- ", font = ("Helvetica 10 bold")).pack(side = LEFT)
-antonym = Label(frame3, text = "", font = ("Helvetica 10"))
-antonym.pack(side = LEFT)
-frame3.pack(pady = 10)
-Button(root, text = "Submit", font = ("Helvetica 15 bold"), conmmand = dict).pack()
-
-# Execute Tkinter 
-root.mainloop()
+# # Frame 3rd
+# frame2 = Frame(root)
+# Label(frame2, text = "Synonym:- ", font= ("Helvetica 10 bold").pack(side = LEFT))
+# synonym = Label(frame2, text = "", font = ("Helvetica 10"))
+# synonym.pack()
+# frame2.pack( pady = 10)
 
 
-# from pydictionary import Dictionary
-# dict = Dictionary("fix")
+# # Frame 4th
+# frame3 = Frame(root)
+# Label(frame3, text = "Meaning:- ", font = ("Helvetica 10 bold")).pack(side = LEFT)
+# antonym = Label(frame3, text = "", font = ("Helvetica 10"))
+# antonym.pack(side = LEFT)
+# frame3.pack(pady = 10)
+# Button(root, text = "Submit", font = ("Helvetica 15 bold"), conmmand = dict).pack()
 
-# meanings_list = dict.meanings()
 
-# synonyms_list = dict.synonyms()
+# # Execute Tkinter 
+# root.mainloop()
 
-# antonyms_list = dict.antonyms()
+import tkinter
+import sys
+import os
 
-# dict = Dictionary("fix",10)
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
 
-# dict.print_meanings("red")
 
-# dict.print_synonyms("green")
+#create main window
+master = tkinter.Tk()
+master.title("tester")
+master.geometry("300x100")
 
-# dict.print_antonyms("blue")
+
+#make a label for the window
+label1 = tkinter.Label(master, text='Hellooooo')
+# Lay out label
+label1.pack()
+
+# Run forever!
+master.mainloop()
