3d33c4d63cae24b07386d89d37641d0aa321b515
Loic <konanloic10@gmail.com>
2021-11-12 18:26:54 -0600

    README.md

--- Python Projects/A03/main.py
+++ Python Projects/A03/main.py
@@ -1,80 +1,29 @@
-# # Import Required Library
-# from tkinter import *
+from tkinter import *
 
-# from PyDictionary import PyDictionary
+fL = {}
 
-# # Create objects
-# dictionary = PyDictionary()
+def commando(x, y):
+    fL.update({x:int(y)})  # Please note that these x and y vars are private to this function.  They are not the x and y vars as defined below.
+    print(fL)
 
-# root = Tk()
+root = Tk()
+root.title("Spam Words")
 
-# # Set geometry
-# root.geometry("400X400")
+x = StringVar()  # Creating the variables that will get the user's input.
+y = StringVar()
 
-# def dict():
-#     meaning.config(text = dictionary.meaning(word.get())['Noun'][0])
-#     meaning.config(text = dictionary.synonym(word.get()))
-#     meaning.config(text = dictionary.antonym(word.get()))
-    
-# # Add Labels, Button and Frames
-# Label(root, text = "Dictionary", font = ("Helvetica 20 bold"), fg = "Green").pack( pady = 10)
+label_1 = Label(root, text="Say a word: ", bg="#333333", fg="white")
+label_2 = Label(root, text="Give it a value, 1-10:", bg="#333333", fg="white")
+entry_1 = Entry(root, textvariable=x)
+entry_2 = Entry(root, textvariable=y)
 
-# # Frame 1st
-# frame = Frame(root)
-# Label(frame, text = "Type Word", font = ("Helvetica 15 bold")).pack(side = LEFT)
+label_1.grid(row=1)
+label_2.grid(row=3)
 
-# word = Entry(frame, font = ("Helvetica 15 bold"))
+entry_1.grid(row=2, column=0)
+entry_2.grid(row=4, column=0)
 
-# word.pack()
-# frame.pack( pady = 10)
+but = Button(root, text="Execute", bg="#333333", fg="white", command=lambda :commando(x.get(), y.get()))  # Note the use of lambda and the x and y variables.
+but.grid(row=5, column=0)
 
-# # Frame 2nd
-# frame1 = Frame(root)
-# Label(frame1, text = "Meaning:- ", font = ("Helvetica 10 bold")).pack(side = LEFT)
-# meaning = Label(frame1, text = "", font = ("Helvetica 10 bold"))
-# meaning.pack()
-# frame1.pack(pady = 10)
-
-
-# # Frame 3rd
-# frame2 = Frame(root)
-# Label(frame2, text = "Synonym:- ", font= ("Helvetica 10 bold").pack(side = LEFT))
-# synonym = Label(frame2, text = "", font = ("Helvetica 10"))
-# synonym.pack()
-# frame2.pack( pady = 10)
-
-
-# # Frame 4th
-# frame3 = Frame(root)
-# Label(frame3, text = "Meaning:- ", font = ("Helvetica 10 bold")).pack(side = LEFT)
-# antonym = Label(frame3, text = "", font = ("Helvetica 10"))
-# antonym.pack(side = LEFT)
-# frame3.pack(pady = 10)
-# Button(root, text = "Submit", font = ("Helvetica 15 bold"), conmmand = dict).pack()
-
-
-# # Execute Tkinter 
-# root.mainloop()
-
-import tkinter
-import sys
-import os
-
-if os.environ.get('DISPLAY','') == '':
-    print('no display found. Using :0.0')
-    os.environ.__setitem__('DISPLAY', ':0.0')
-
-
-#create main window
-master = tkinter.Tk()
-master.title("tester")
-master.geometry("300x100")
-
-
-#make a label for the window
-label1 = tkinter.Label(master, text='Hellooooo')
-# Lay out label
-label1.pack()
-
-# Run forever!
-master.mainloop()
+root.mainloop()
