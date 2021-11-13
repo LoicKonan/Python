7a84f037512355ffad7b3686585ef87111cf0d30
Loic <konanloic10@gmail.com>
2021-11-12 19:37:29 -0600

    README.md

--- Python Projects/A04/VigenereCipher.py
+++ Python Projects/A04/VigenereCipher.py
@@ -25,6 +25,7 @@ f1.pack(side = LEFT)
 # ==============================================
 #				 TIME
 # ==============================================
+
 localtime = time.asctime(time.localtime(time.time()))
 
 lblInfo = Label(Tops, font = ('helvetica', 50, 'bold'),
