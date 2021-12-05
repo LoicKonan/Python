14ec51b41a7ff039aa8cc4ba7329ca9eacc6c442
Loic <konanloic10@gmail.com>
2021-12-04 23:45:43 -0600

    README.md

--- Python Projects/A03/CarFuel.py
+++ Python Projects/A03/CarFuel.py
@@ -17,49 +17,54 @@ Object-Oriented Programming
 Class: Car
 '''
 class Car:
-    #A car has a certain fuel efficiency (measured in miles/gallon)
-    #and a certain amount of fuel in the gas tank.
+    # A car has a certain fuel efficiency (measured in miles/gallon)
+    # and a certain amount of fuel in the gas tank.
     def __init__(self, efficiency, tanksize):
-        #The efficiency is specified in the constructor,
+        
+        # The efficiency is specified in the constructor,
         # and the initial fuel level is 0.
         self.__fuelLevel = 0
+       
         # measured in miles/gallon
         self.__efficiency = efficiency
         self.__tankSize = tanksize
+            
+    # returns current fuel level
+    def getGasLevel(self):
+        return self.__fuelLevel
+    
+    # adds gas to the tank and returns how many gallons added
+    def addGas(self, level):
+    
+        # if the added fuel exceeds the tank size,
+        # have the full tank gas
+        if self.__fuelLevel == self.__tankSize:
+            print("Tank is already full")
+            return 0
         
-# returns current fuel level
-def getGasLevel(self):
-    return self.__fuelLevel
-  
-# adds gas to the tank and returns how many gallons added
-def addGas(self, level):
-   
-    # if the added fuel exceeds the tank size,
-    # have the full tank gas
-    if self.__fuelLevel == self.__tankSize:
-        print("Tank is already full")
-        return 0
+        elif self.__fuelLevel + level > self.__tankSize:
+            print("Gas Added: ", self.__tankSize - self.__fuelLevel)
+            self.__fuelLevel = self.__tankSize
+            return self.__tankSize - self.__fuelLevel
+       
+        else:
+            print("Gas Added: ", level)
+            self.__fuelLevel += level
+            return level
     
-    elif self.__fuelLevel + level > self.__tankSize:
-        print("Gas Added: ", self.__tankSize - self.__fuelLevel)
-        self.__fuelLevel = self.__tankSize
-        return self.__tankSize - self.__fuelLevel
-    else:
-        print("Gas Added: ", level)
-        self.__fuelLevel += level
-        return level
-  
-def drive(self, distance):
-    # calculate gallons required to travel the given distance
-    gallonsRequired = distance / self.__efficiency
-    if self.__fuelLevel - gallonsRequired < 0:
-        print("Car does not have enough gas to drive: ", distance)
-    
-    else:
-        # Reduce the consumed gallon
-        self.__fuelLevel -= gallonsRequired
-        milesToDrive = self.__fuelLevel * self.__efficiency
-        print("You drove ", distance, " miles. You can drive another ", milesToDrive, "on this gas")
+    def drive(self, distance):
+       
+        # calculate gallons required to travel the given distance
+        gallonsRequired = distance / self.__efficiency
+        if self.__fuelLevel - gallonsRequired < 0:
+            print("Car does not have enough gas to drive: ", distance)
+        
+        else:
+            # Reduce the consumed gallon
+            self.__fuelLevel -= gallonsRequired
+            milesToDrive = self.__fuelLevel * self.__efficiency
+            print("You drove ", distance, " miles. You can drive another ", milesToDrive, "on this gas")
+
 
 #Prints the menu and get user choice
 def getMenuChoice():
@@ -81,13 +86,16 @@ if __name__ == "__main__":
     with open(fileName, 'r') as f:
         first_line = f.readline().rstrip('\n').strip().split(':')
         second_line = f.readline().rstrip('\n').strip().split(':')
+        
         efficiency = int(first_line[1])
         tanksize = int(second_line[1])
         
         myHybrid = Car(efficiency, tanksize) # 20 miles per gallon
         myHybrid.addGas(25)
+       
         myHybrid.drive(100) # Drive 100 miles
         print("Fuel level: ", myHybrid.getGasLevel()) # Print fuel remaining
+       
         myHybrid.addGas(9) # Add 9 gallons
         print("Fuel level: ", myHybrid.getGasLevel()) # Print fuel remaining
         
@@ -96,6 +104,7 @@ if __name__ == "__main__":
     while choice != 4:
         # get user choice
         choice = getMenuChoice()
+       
         # See Cureent Fuel
         if choice == 1:
             logMessage("User Input: 1 - See Current Fuel Level")
