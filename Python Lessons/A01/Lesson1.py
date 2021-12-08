c4f9509b23273600bd20b39dac753e3a56228955
Loic <konanloic10@gmail.com>
2021-12-08 03:02:45 -0600

    README.md

--- Python Lessons/A01/Lesson1.py
+++ Python Lessons/A01/Lesson1.py
@@ -1,9 +1,3 @@
-def pay(Hours, rate):
-    if(Hours < 41) and Hours > 0:
-        return Hours * rate
-    elif (Hours > 40) and Hours > 0:
-        return (40 * rate) + ((Hours - 40) * 15)
-    else:
-        return('Invalid hours')
-    
-print(pay(45,10))
+Ondra = "Lying ass nigger"
+
+print(Ondra.replace('nigger', 'man'))
