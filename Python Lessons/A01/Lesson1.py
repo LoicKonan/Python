95be9fd99937d4c2f12fdfe76d494301a9d56e89
Loic <konanloic10@gmail.com>
2022-09-17 13:33:48 -0500

    README.md

--- Python Lessons/A01/Lesson1.py
+++ Python Lessons/A01/Lesson1.py
@@ -1,8 +1,33 @@
-x = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
+# Program Heading
+print("This program calculates the total cost of an order and prints a billing statement");
 
-a = x.split()
+# Tax Rate
+TAX_RATE = 0.0825
 
-b = a[1]
+# Input Statements
+Cost = float(input("Widget Cost (Please Enter Dollar Amount):"));
+Quantity = float(input("# of Widgets to be Purchased (Please Enter Whole Number):"));
+Discount_Rate = float(input("Discount to be Applied (Please Enter Decimal Value):"));
 
-b = b.split('@')
-print(b[1])
+# Calculations
+Sub_1 = Cost * Quantity;
+Discount = Sub_1 * Discount_Rate;
+Sub_2 = Sub_1 - Discount;
+Tax = Sub_2 * TAX_RATE;
+Total = Sub_2 + Tax;
+
+# 2 Decimal Points
+Sub_1 = "{:.2f}".format(Sub_1);
+Discount = "{:.2f}".format(Discount);
+Sub_2 = "{:.2f}".format(Sub_2);
+Tax = "{:.2f}".format(Tax);
+Total = "{:.2f}".format(Total);
+
+# Billing Statement
+print("Subtotal Before Discount: $ {:>8}".format(Sub_1));
+print("Discount Applied:         $ {:>8}".format(Discount));
+print("Subtotal After Discount:  $ {:>8}".format(Sub_2));
+print("Tax:                      $ {:>8}".format(Tax));
+print("Final Total:              $ {:>8}".format(Total));
+
+# print("Kyle Hackett");
