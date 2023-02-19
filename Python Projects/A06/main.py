7f4c164019ad05211e6ee3712803b13d7abb2c4d
Loic <konanloic10@gmail.com>
2023-02-18 18:01:00 -0600

    README.md

--- Python Projects/A06/main.py
+++ Python Projects/A06/main.py
@@ -1,13 +1,5 @@
-import sys
-# import numpy as np
-# import pandas as pd
-# from sklearn import ...
-
-for line in sys.stdin:
-    print(line, end="")
-    
-def square_int(n):
-    return n ** 2
-
-square_int(5)
-print(square_int)
+# Build a worlde game in pygame.
+# This game should have flying in of letters when a word is entered
+# display a keyboard on the screen
+# display a loser to the screen if the player lose with a loser sound and ask him if he want to play again
+# or if the player win it should display winner with a winner sound and if the player want to play again. 
